20bbbd96040b42e46cab6585c8f56a34fbba8b0d
Yacine Hmito <yacine.hmito@gmail.com>
2017-08-28 06:45:20 +0200

    Testing websockets

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -4,6 +4,14 @@
     <p v-if="serverMessage">
       The server sends from HTTP: {{ serverMessage }}
     </p>
+    <template v-if="ws">
+      <ul v-if="wsMessages.length > 0">
+        <li v-for="(msg, index) in wsMessages" :key="index">
+          {{ msg }}
+         </li>
+      </ul>
+    </template>
+    <button @click="sendMessage()">Send message</button>
   </section>
 </template>
 
@@ -11,13 +19,34 @@
 export default {
   data() {
     return {
-      serverMessage: ''
+      serverMessage: '',
+      wsMessages: [],
+      ws: null,
+      counter: 0
     }
   },
   created() {
     this.$axios.get('/hello').then(response => {
       this.serverMessage = response.data.message
     })
+    if (process.browser) {
+      const ws = new window.WebSocket('ws://localhost:3000')
+      console.log(ws)
+      ws.onopen = () => {
+        this.ws = ws
+      }
+      ws.onmessage = msg => {
+        this.wsMessages.push(msg.data)
+      }
+    }
+  },
+  methods: {
+    sendMessage() {
+      if (this.ws) {
+        this.ws.send(`${this.counter}`)
+        this.counter++
+      }
+    }
   }
 }
 </script>
--- server/app.js
+++ server/app.js
@@ -2,12 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 import router from './router'
+import expressWs from '@yacinehmito/express-ws'
 
 const app = express()
+expressWs(app)
 
 app.use(cors())
 app.use(logger('dev'))
 
 app.use('/api', router)
+
+let counter = 0
+app.ws('/', (ws, req) => {
+  ws.on('message', msg => {
+    ws.send(`Server's message n° ${counter}; Client's message n° ${msg}`)
+    counter++
+  })
+})
 
 export default app
